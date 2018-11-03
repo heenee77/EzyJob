e51dc3186135efe3be62118834aad20dcfb7d461
Hee Nee <heenee97@yahoo.com>
2018-11-04 03:16:32 +0800

    Dashboard Update 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  jobs: Array<{jobNum: string, jobDesc: string}>;
+
   constructor(public navCtrl: NavController) {
-
+    this.jobs = [
+      {jobNum: "J0001", jobDesc:"Initial Meeting"},
+      {jobNum: "J0002", jobDesc:"Planning"},
+      {jobNum: "J0003", jobDesc:"Development"}
+    ]
   }
-
+  
 }
